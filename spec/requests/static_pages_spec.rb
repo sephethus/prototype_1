ee8aa2465cf67272c057a957470b4b9e6026fedf
Nathan <nathan.mckaskle@gmail.com>
2012-11-15 12:00:42 -0500

    CSS issues

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,6 +11,7 @@ describe "StaticPages" do
 		before { visit root_path }
 
 		it { should have_selector('title', text: full_title('Home')) }
+		it { should have_selector('h1', text: "TEST") }
 	end
 
 end
