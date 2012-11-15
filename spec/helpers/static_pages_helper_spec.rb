3e3847cba03acf2dea25c760c2febda7fbf78b87
Nathan <nathan.mckaskle@gmail.com>
2012-11-15 10:04:57 -0500

    Home page and tests

--- spec/helpers/static_pages_helper_spec.rb
+++ spec/helpers/static_pages_helper_spec.rb
@@ -1,12 +1,10 @@
-module ApplicationHelper
-	
-	def full_title(page_title)
+require 'spec_helper'
+
+def full_title(page_title)
 		base_title = "Prototype One"
 		if page_title.empty?
 			base_title
 		else
 			"#{base_title} | #{page_title}"
 		end
-	end
-	
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+
+
+describe "StaticPages" do
+
+	subject { page }
+
+	describe "Test layout." do
+
+		before { visit root_path }
+
+		it { should have_selector('title', text: full_title('Home')) }
+	end
+
+end
